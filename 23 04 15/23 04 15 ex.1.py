156261b19ca1cc49a5222b52c3a4ff42ed4d792b
lellhu <130810894+lellhu@users.noreply.github.com>
2023-04-15 12:23:27 +0900

    Add files via upload

---
+++ 23 04 15/23 04 15 ex.1.py
@@ -0,0 +1,15 @@
+'''
+2023.04.15
+정우진
+변수, 출력문-print() 연습
+'''
+
+print('파이썬 프로그램')
+print("출력문 만들기")
+print() #한 줄 띄우기
+print("'python'")
+
+age=14 #age변수에 나이(정수)로 저장
+name='jwoojin' #name변수에 이름(문자)지정
+
+print('이름은 {}이고, 나이는 {}입니다.'.format(name,age))
