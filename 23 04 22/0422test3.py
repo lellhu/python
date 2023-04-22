5c8fe2e1c2a87396461b9c041034cf90690f7c25
lellhu <130810894+lellhu@users.noreply.github.com>
2023-04-22 12:50:14 +0900

    Add files via upload

---
+++ 23 04 22/0422test3.py
@@ -0,0 +1,29 @@
+'''
+2023 04 22
+정우진
+#문제
+    num1,num2 변수를 이용한 사칙연산 프로그램
+#문제분석
+    변수-num1,num2
+    수식
+        num1=num2,num1-num2,num1*num2,num1/num2
+#알고리즘
+    1.변수 선언
+           num1=10
+           num2=20
+    2.사칙연산 계산
+           num1+num2
+           num1-num2
+           num1*num2
+           num1/num2
+    3.결과 출력
+'''
+
+num1=10
+num2=20
+
+print('{}+{}={}'.format(num1,num2,num1+num2))
+print('{}-{}={}'.format(num1,num2,num1-num2))
+print('{}*{}={}'.format(num1,num2,num1*num2))
+print('{}/{}={}'.format(num1,num2,num1/num2))
+ 
